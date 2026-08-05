906127b50ebf84c2b5ac215992028097988c44cc
Vishal Yadav <vishal70563@gmail.com>
2026-08-05 23:39:35 +0530

    First commit

---
+++ peter.py
@@ -0,0 +1,79 @@
+#Question.11
+# food=input("food:")
+# eat="yes"if food =="cake" else"no"
+# print(eat)
+
+#Question.12
+# food = input("food:")
+# print("sweet")if food =="cake" or food =="jalebi" else print("not sweet")
+
+#Question.13
+# age =int(input("Enter the age:"))
+# vote =("yes","no")[age<18]
+
+#Question.14
+# sal =float(input("salary:"))
+# tax =sal*(0.1, 0.2)["salary:"]
+# print(tax)
+
+#Question.15
+# p =float(input("enter the principle:"))
+# r =float(input("enter the rate:"))
+# t =float(input("enter the time:"))
+
+# print("simple interest",si =(p*r*t)/100)
+
+#Question.15
+# a = int(input("Enter the first name\n:"))
+# b = int(input("Enter the second name\n:"))
+
+# print("addition:",a+b)
+# print("sub:",a-b)
+# print("multiplication:",a*b)
+# print("division:",a/b)
+# print("remainder:",a%b)
+# print("square:",a**b)
+
+#Question.16
+# a = int(input("Enter the first name:"))
+# b = int(input("Enter the second name:"))
+
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+# print(a>b)
+# print(a<b)
+
+
+#Question.17
+# a = int(input("Enter the first name:"))
+# a = int(input("Enter the second name:"))
+# a=a,a**a
+# print(a)
+
+# num =10
+# num **=5
+# print("num:",num)
+
+
+#Question.18
+# a =50
+# b =30
+
+# print(not false)
+# print(not (a>b))
+# val1 =False
+# val2 =False
+
+# print("AND operator:",vall and val2)
+# print("OR operator:",vall or val2)
+
+#Question.19
+# a = 3.14
+# a = str(a)
+# print(type(a))
+
+#Question.20
+name=(input("enter your age:"))
+print(type(name),name)
