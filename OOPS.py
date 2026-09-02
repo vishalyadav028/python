8086d96a6561cfc630e78388a211617fdded55dd
Vishal Yadav <vishal70563@gmail.com>
2026-09-02 23:36:47 +0530

    Updated Python files

--- OOPS.py
+++ OOPS.py
@@ -27,18 +27,120 @@
 
 # d1.sound()
 # c1.sound()
-class Vehicle:
-    color = "black"
-    petroldesile = "petrol"
-    mileage = "10"
 
 
-car = Vehicle()
-print(car.color)
 
-bike = Vehicle()
-print(bike.color)
 
-aeroplane = Vehicle()
-print(aeroplane.mileage)
-print(aeroplane.color)
+
+
+
+
+
+
+
+
+
+# STAR OF OOPS
+
+
+# class Vehicle:
+#     color = "black"
+#     petroldesile = "petrol"
+#     mileage = "10"
+
+
+# car = Vehicle()
+# print(car.color)
+
+# bike = Vehicle()
+# print(bike.color)
+
+# aeroplane = Vehicle()
+# print(aeroplane.mileage)
+# print(aeroplane.color)
+
+
+
+
+
+
+# class car:
+#     brand="vishal"
+# obj1= car()
+# print("brand name is",obj1.brand)
+
+
+
+
+
+# class laptop:
+#     brand="default"
+#     Ram="8GB"
+#     Price="1 lakh"
+
+# laptop1 =laptop()
+# laptop1.brand="macbook"
+# laptop1.Ram="16GB"
+# laptop1.Price="72000"
+# print("laptop brand,Ram",laptop1.brand,"price",laptop.Price)
+
+# laptop2 =laptop()
+# laptop2.brand="lenovo"
+# print("laptop brand",laptop2.brand)
+
+
+
+
+
+
+
+# class laptop:
+#     brand="dafualt"
+#     Ram="8GB"
+#     Price="1 lakh"
+
+# pc=laptop() 
+# pc.brand="Mackbook"
+# pc.Ram="16GB"
+# print("brand name pc: ",pc.brand)
+
+# pc2=laptop()
+# pc.brand="lenovo"
+# print("brand name pc2: ",pc2.brand)
+
+
+
+
+
+
+# class student:
+#     schoolname="abc school"
+
+#     def __init__(self):
+#         print("what is your name:")
+#         print(self)
+
+# studen1=student()
+# print(studen1.schoolname)  
+
+# studen2=student()
+
+
+
+
+
+
+
+
+
+class student:
+    def __init__(self,name,marks):
+        self.name =name
+        self.marks =marks
+        print("adding new student")
+
+s1 =student("vishal",35)
+print(s1.name, s1.marks)
+
+s2 =student("ritik",40)
+print(s2.name,s2.marks)
