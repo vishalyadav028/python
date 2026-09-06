e53d85d1897a5b0e45e50ad021a21e09aa9a69a1
Vishal Yadav <vishalyadav70563@gmail.com>
2026-09-06 21:58:47 +0530

    python

--- other.py
+++ other.py
@@ -1,29 +1,61 @@
+# QUESTION===1
 # x=2+3j
+# print(x)
+
+# a =int(input("Enter the first number: "))
+# b =int(input("enter the seoncd number: "))
+# x=complex (a+b)
 # print(x)
 
 
 
+
+
+# QUESTION===2
 # fruit=["apple","banana","mango"]
 # print(fruit[0])
 
+# fruit = ["apple", "banana", "mango"]
+# choice = int(input("Enter index: "))
+# print("Your fruit:", fruit[choice])
 
 
+
+
+
+# QUESTION===3
 # student =("vishal",17,"BCA")
 # print(student)
 # print(student[1])
-# print(student[2])
+
+# student=("vishal",18,"mca")
+# std=int(input("enter the index no: "))
+# print("your tuple",student[std],type(student))
 
 
 
+
+
+
+# QUESTION===4
 # number={10,20,30}
 # print(number)
 # number.remove(30)
 # number.add(60)
 # print(number)
 
+# num={20,40,20,45,11,'vishal'}
+# print("oringal sets: ",num)
+# num1 =int(input("enter the remove no..."))
+# num.remove(num1)
+# num2= int(input("enter the add values..."))
+# num.add(num2)
+# print("your set",num)
+# print(type(num))
 
 
 
+# QUESTION===5
 # student={
 #     "name":"vishal",
 #     "age":18,
@@ -36,97 +68,201 @@
 # student["city"]="bhopal"
 # print(student)
 # print(student["name"])
+# student={
+#     "name":input("enter the name..."),
+#     "age":int(input("enter the age...")),
+#     "course":input("enter the course...")
+# }
+# print("this is student data---",student)
+# print("____________________________________________________________")
 
-     
-
-# print("hello")
-# print("10/0")
-# print("bye")
-
+# student["age"]=int(input("enter new age..."))
+# student["city"]=input("enter your city...")
+# print("uptate student date",student)
+# print("your name",student["name"])
 
 
+
+
+
+# QUESTION===6
 # a= 3.14
 # a= str(a)
 # print(a)
 # print(type(a))
+# a =float(input("enter the decimal number: "))
+# a=str(a)
+# print("convert",a,"data type====",type(a))
 
 
+
+
+
+# QUESTION===7
+# val="vishal"
+# print(val,type(val))
 
 # val =input("enter some values: ")
 # print(type(val),val)
 
+
+
+
+
+# QUESTION===8
+# first="vishal"
+# second=" yadav"
+# print("added name: ",first + second)
 
 # first =int(input("enter the first no: "))
 # second =int(input("enter the second no: "))
 # print("sum =",first + second)
 
 
+
+
+
+
+
+# QUESTION===9
+# side=2.0
+# print("area ",side**2)
+
 # side = float(input("enter square side: "))
 # print("area =",side **2)
 
+
+
+
+
+# QUESTION===10
+# a= 20
+# b= 12
+# print("average caluate: ",(a+b)/2)
 
 # a= float(input("enter first: "))
 # b= float(input("enter second: "))
 # print("avg =",(a+b)/2)
 
 
-# print("vishal yadav\n"*10)
 
+
+
+
+# QUESTION===11
+# print("vishal yadav\n"*10)
+# a=input("enter your name: ")
+# print((a + "\t") * 10)
+
+
+
+
+# QUESTION===12
 # name= "vishal kumar"
 # age1= 25
 # age2= 40
 # print("actual values:",age1)
-# favsubject ="maths"
+# subject ="maths"
 # age2 =age1
 # print("changed values:",age2)
 
+# name= input("enter your name...")
+# age1=int(input("enter your age1..."))
+# age2=int(input("enter your age2..."))
+# print("Actual values: ",age1)
+# sub= input("enter your subject name: ")
+# age2=age1
+# print("changed values",age2)
 
+
+
+
+
+
+# QUESTION===13
+# a= 10
+# b= 20
+# print("compare",a>b)
 # a= int(input("enter first: "))
-# b= int(input("enter second"))
+# b= int(input("enter second: "))
 # print(a>b)
 
 
+
+
+
+# QUESTION===14
 # diameter =int(input("inter the values of daimeter: "))
 # radius = diameter/2
 # area =3.14 *(radius **2)
 # print("area of circle is",area)
 
+# diameter= 25
+# radius= diameter/2
+# area =3.14 *(radius **2)
+# print("area of circle is",area)
 
+
+
+
+# QUESTION===15
 # age =input("enter your age:")
 # print("values entered is:",age)
 # print("data type is:",type(age))
 
+# age= 18
+# print("age ka types:",age)
+# print("data types is",type(age))
 
+
+
+
+
+# QUESTION===19
 # input1= int(input("enter first number:"))
 # input2= int(input("enter second number:"))
 # sum= input1 + input2
 # print("sum of the 2 values is",sum)
 
+# input1= 12
+# input2= 3
+# print("sum of two number",input1+input2)
 
 
+
+
+
+
+# QUESTION===20
 # x= 5
 # y= 10.4
 # z= x+y
 # print("answer",z)
 # print("data type of answer is",type(z))
+# x= int(input("enter the first number: "))
+# y= int(input("enter the second number: "))
+# print("sum krna aand data types====",x+y,type(x+y))
 
 
+
+
+
+
+# QUESTION===21
 # num= input("enter a values:")
 # converted= float(num)
 # print("oringal values",type(num),num)
 # print("converted values",converted,type(converted))
 
-
-
-# bhi yah mujhe doubt hai yah question
-# a=int(input("enter temperature in celsius:"))
-# fahrenheit =(a *9/5)+2
-# kelvin =a + 273.15
-# print("fahrenheit",fahrenheit)
-# print("kelvin",kelvin)
+# num= "56"
+# num1= float(num)
+# print("Oringal values____",num,type(num))
+# print("changed values____",num1,type(num1))
 
 
 
+
+# ------------------- QUESTION BANK-------------------------#
 # Question 1.1 
 # Numbers a = 20, b = 5 lekar addition, subtraction, multiplication, division aur modulus print karo.
 # a= 20
@@ -137,12 +273,27 @@
 # print("division",a/b)
 # print("modulus",a%b)
 
+# a= int(input("enter the first number:"))
+# b= int(input("enter the second number:"))
+# print("addition",a+b)
+# print("subtraction",a-b)
+# print("multiplication",a*b)
+# print("division",a/b)
+# print("modulus",a%b)
 
 
 # Question 1.2 
 # a = 15, b = 4 lekar all arithmetic operators ka result nikalo.
 # a= 15
 # b= 4
+# print("addition",a+b)
+# print("subtraction",a-b)
+# print("multiplication",a*b)
+# print("division",a/b)
+# print("modulus",a%b)
+
+# a= int(input("enter the first number:"))
+# b= int(input("enter the second number:"))
 # print("addition",a+b)
 # print("subtraction",a-b)
 # print("multiplication",a*b)
@@ -162,17 +313,34 @@
 # print("this is equal equal: ",a==b)
 # print("this is not equal: ",a!=b)
 
+# a= int(input("enter the first values:"))
+# b= int(input("enter the second values:"))
+# print("this is grater than:",a>b)
+# print("this is less than:",a<b)
+# print("this is grater than equal:",a>=b)
+# print("this is less than equal:",a<=b)
+# print("this is ==",a==b)
+# print("this is !=",a!=b)
+
 
 
 
 # Question 1.4 
 # age = 18 aur hasID = True lekar logical AND (and / &&) use karke check karo ki person allowed hai ya nahi.
 # age =1
-# ID =True
-# if(age >= 18 and ID):
+# hasID =True
+# if(age >= 18 and hasID):
 #     print("person is allowed")
 # else:
-#     print("person is not allowed")  
+#     print("person is not allowed") 
+ 
+# age= int(input("enter the age: "))
+# hasID= True
+# if(age >=18 and hasID):
+#     print("person is allowed")
+# else:
+#     print("person is not allowed")    
+
 
 
 
@@ -182,7 +350,13 @@
 # if(a %2==0):
 #     print("this is even number")
 # else:
-#     print("this is odd number")    
+#     print("this is odd number")
+#   
+# a =int(input("enter the number: "))
+# if(a %2 ==0):
+#     print("this is even number")
+# else:
+#     print("this is odd number")      
 
 
 
@@ -193,6 +367,12 @@
 # city ="Raipur"
 # print("my name is",name,"and i am",age,"year old my fav",city,"")
 
+# name =input("enter your name: ")
+# age= int(input("enter your age: "))
+# city= input("enter your city: ")
+# print("my name is",name)
+# print("i am ",age,"year old")
+# print("i am from",city)
 
 
 
@@ -201,6 +381,12 @@
 # a= 10
 # b= 20
 # print("total sum",a+b)
+
+# a= int(input("enter the 1st number:"))
+# b= int(input("enter the 2nd number:"))
+# print("total number calculate",a+b)
+
+
 
 
 
@@ -211,6 +397,13 @@
 # area =length * breath
 # print("rectangle of area:",area)
 
+# length= int(input("enter the number:"))
+# breadth= int(input("enter the number:"))
+# area= length * breadth
+# print("rantagle of area:",area)
+
+
+
 
 
 
@@ -220,6 +413,14 @@
 # quantity= 3
 # print("otal bill calculate",price * quantity)
 
+# price =int(input("enter the price: "))
+# quantity=int(input("enter the quantity: "))
+# print("total bill calculate: ",price *quantity)
+
+
+
+
+
 
 
 # Question 2.5 
@@ -228,6 +429,15 @@
 # print("values of before:",score)
 # score= 100
 # print("values of after:",score)
+
+# score= int(input("enter the before values: "))
+# print("before",score)
+# score= int(input("enter the after values: "))
+# print("after values",score)
+
+
+
+
 
 
 
@@ -242,6 +452,16 @@
 # print("my mystr:",type(mystr))
 # print("my bool",type(mybool))
 
+# myint= int(input("enter your integer number: "))
+# myfloat= float(input("enter your floating number: "))
+# mystr=input("enter your sring name: ")
+# mybool= input("enter your boolean:").lower() == "true"
+
+# print("integer number:",type(myint))
+# print("my float number:",type(myfloat))
+# print("my mystr:",type(mystr))
+# print("my bool",type(mybool))
+
 
 
 
@@ -250,6 +470,9 @@
 # age =int(input("entr the age: "))
 # print("i am ",age," year old")
 # print("eska type hai yah: ",type(age))
+
+# age=18
+# print("your age:",age,"and datatype",type(age))
 
 
 
@@ -260,6 +483,15 @@
 # print("convert",num)
 # print(convert+50)
 
+# num = input("Enter the str number: ")
+# convert = int(num)
+# print("Converted:", convert)
+# added = int(input("Enter the number to add: "))
+# print("Added number:", convert+ added)
+
+
+
+
 
 
 # Question 3.4 
@@ -267,27 +499,108 @@
 # price= 99.5
 # convert= int(price)
 # print("converted values:",convert)
+# a= float("enter the number:")
+# b= int(a)
+# print("converted",b)
 
 
 
 
 # Question 3.5
 # Ek list, tuple, dictionary aur set banao aur sabka type() print karo.
-list1=["mungdal","chana","aalu"]
-print("this is list",list1,"yah eska types hai",type(list1))
+# list1=["mungdal","chana","aalu"]
+# print("this is list",list1,"yah eska types hai",type(list1))
 
 
-tuple1=("apple","banana","papaye")
-print("this is tuple",tuple1,"yah eska types hai",type(tuple1))
+# tuple1=("apple","banana","papaye")
+# print("this is tuple",tuple1,"yah eska types hai",type(tuple1))
 
 
-dictionary={
-    "name":"vishal",
-    "age":18,
-    "village":"bihar"
-}
-print("this is dictionary",dictionary,"yah eska types hai",type(dictionary))
+# dictionary={
+#     "name":"vishal",
+#     "age":18,
+#     "village":"bihar"
+# }
+# print("this is dictionary",dictionary,"yah eska types hai",type(dictionary))
 
 
-set1={"rotti","chaval","tea"}
-print("this is set",set1,"yah types",type(set1))
+# set1={"rotti","chaval","tea"}
+# print("this is set",set1,"yah types",type(set1))
+
+
+
+
+
+# Question 4.1 
+# Number positive, negative ya zero hai, check karo.
+# a =0
+# if(a>0):
+#     print("this is positive number")
+# elif(a<0):
+#     print("this is negative number") 
+# else:
+#     print("this is zero")       
+
+
+
+
+# Question 4.2
+# Number even ya odd hai, check karo.
+# a =18
+# if(a &2==0):
+#     print("this is even number")
+# else:
+#     print("this is odd number")  
+
+
+
+
+# Question 4.3 
+# User ki age lekar check karo: 18 ya usse zyada → Eligible, warna Not Eligible.
+# age =17
+# if(age >= 18):
+#     print("eligible for voter")
+# else:
+#     print("not eligible for voter")    
+
+
+
+
+
+# Question 4.4 
+# Teen numbers mein se sabse bada number find karo.
+# a=10
+# b=1
+# c=2
+# if(a>=b and a>=c):
+#     print("first largest number")
+# elif(b>=a and b>=c):
+#     print("second largest number")
+# else:        
+#     print("third largest number")
+
+
+
+
+# Question 4.5 
+# Marks lekar grade print karo (90+ = A, 75–89 = B, 60–74 = C, 40–59 = D, <40 = Fail).
+# marks=29
+# if(marks>=90):
+#     print("Grade --'A'")
+# elif(marks>=75):
+#     print("Grade--B")
+# elif(marks>=64):
+#     print("Grade--c")
+# elif(marks>=40):
+#     print("Grade--D")
+# elif(marks>=30):
+#     print("Result is Passed")  
+# else:
+#     print("Result is Failled")       
+
+
+
+
+
+# Question 5.1 
+# 1 se 10 tak numbers print karo.
