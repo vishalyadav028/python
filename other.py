5d7775f1765daf67dd6afaa0e501d5a88c3e59c3
Vishal Yadav <vishalyadav70563@gmail.com>
2026-09-07 22:50:09 +0530

    Added Python code

--- other.py
+++ other.py
@@ -1,606 +1,199 @@
-# QUESTION===1
-# x=2+3j
-# print(x)
+# a = 20
+# b = 5
 
-# a =int(input("Enter the first number: "))
-# b =int(input("enter the seoncd number: "))
-# x=complex (a+b)
-# print(x)
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
+# print("Modulus:", a % b)
 
 
 
 
 
-# QUESTION===2
-# fruit=["apple","banana","mango"]
-# print(fruit[0])
 
-# fruit = ["apple", "banana", "mango"]
-# choice = int(input("Enter index: "))
-# print("Your fruit:", fruit[choice])
+# a = 15
+# b = 4
 
+# print("Sum:", a + b)
+# print("Diff:", a - b)
+# print("Prod:", a * b)
+# print("Div:", a / b)
+# print("Floor Div:", a // b)
+# print("Mod:", a % b)
 
 
 
 
-# QUESTION===3
-# student =("vishal",17,"BCA")
-# print(student)
-# print(student[1])
 
-# student=("vishal",18,"mca")
-# std=int(input("enter the index no: "))
-# print("your tuple",student[std],type(student))
+# a = 10
+# b = 20
 
+# print("a > b:", a > b)
+# print("a < b:", a < b)
+# print("a >= b:", a >= b)
+# print("a <= b:", a <= b)
+# print("a == b:", a == b)
+# print("a != b:", a != b)
 
 
 
 
 
-# QUESTION===4
-# number={10,20,30}
-# print(number)
-# number.remove(30)
-# number.add(60)
-# print(number)
 
-# num={20,40,20,45,11,'vishal'}
-# print("oringal sets: ",num)
-# num1 =int(input("enter the remove no..."))
-# num.remove(num1)
-# num2= int(input("enter the add values..."))
-# num.add(num2)
-# print("your set",num)
-# print(type(num))
 
+# age = 18
+# has_id = True
 
-
-# QUESTION===5
-# student={
-#     "name":"vishal",
-#     "age":18,
-#     "course":"BCA",
-#     "course":"BCA"
-# }
-# print(student["name"])
-# student["age"]=32
-# print(student)
-# student["city"]="bhopal"
-# print(student)
-# print(student["name"])
-# student={
-#     "name":input("enter the name..."),
-#     "age":int(input("enter the age...")),
-#     "course":input("enter the course...")
-# }
-# print("this is student data---",student)
-# print("____________________________________________________________")
-
-# student["age"]=int(input("enter new age..."))
-# student["city"]=input("enter your city...")
-# print("uptate student date",student)
-# print("your name",student["name"])
-
-
-
-
-
-# QUESTION===6
-# a= 3.14
-# a= str(a)
-# print(a)
-# print(type(a))
-# a =float(input("enter the decimal number: "))
-# a=str(a)
-# print("convert",a,"data type====",type(a))
-
-
-
-
-
-# QUESTION===7
-# val="vishal"
-# print(val,type(val))
-
-# val =input("enter some values: ")
-# print(type(val),val)
-
-
-
-
-
-# QUESTION===8
-# first="vishal"
-# second=" yadav"
-# print("added name: ",first + second)
-
-# first =int(input("enter the first no: "))
-# second =int(input("enter the second no: "))
-# print("sum =",first + second)
-
-
-
-
-
-
-
-# QUESTION===9
-# side=2.0
-# print("area ",side**2)
-
-# side = float(input("enter square side: "))
-# print("area =",side **2)
-
-
-
-
-
-# QUESTION===10
-# a= 20
-# b= 12
-# print("average caluate: ",(a+b)/2)
-
-# a= float(input("enter first: "))
-# b= float(input("enter second: "))
-# print("avg =",(a+b)/2)
-
-
-
-
-
-
-# QUESTION===11
-# print("vishal yadav\n"*10)
-# a=input("enter your name: ")
-# print((a + "\t") * 10)
-
-
-
-
-# QUESTION===12
-# name= "vishal kumar"
-# age1= 25
-# age2= 40
-# print("actual values:",age1)
-# subject ="maths"
-# age2 =age1
-# print("changed values:",age2)
-
-# name= input("enter your name...")
-# age1=int(input("enter your age1..."))
-# age2=int(input("enter your age2..."))
-# print("Actual values: ",age1)
-# sub= input("enter your subject name: ")
-# age2=age1
-# print("changed values",age2)
-
-
-
-
-
-
-# QUESTION===13
-# a= 10
-# b= 20
-# print("compare",a>b)
-# a= int(input("enter first: "))
-# b= int(input("enter second: "))
-# print(a>b)
-
-
-
-
-
-# QUESTION===14
-# diameter =int(input("inter the values of daimeter: "))
-# radius = diameter/2
-# area =3.14 *(radius **2)
-# print("area of circle is",area)
-
-# diameter= 25
-# radius= diameter/2
-# area =3.14 *(radius **2)
-# print("area of circle is",area)
-
-
-
-
-# QUESTION===15
-# age =input("enter your age:")
-# print("values entered is:",age)
-# print("data type is:",type(age))
-
-# age= 18
-# print("age ka types:",age)
-# print("data types is",type(age))
-
-
-
-
-
-# QUESTION===19
-# input1= int(input("enter first number:"))
-# input2= int(input("enter second number:"))
-# sum= input1 + input2
-# print("sum of the 2 values is",sum)
-
-# input1= 12
-# input2= 3
-# print("sum of two number",input1+input2)
-
-
-
-
-
-
-# QUESTION===20
-# x= 5
-# y= 10.4
-# z= x+y
-# print("answer",z)
-# print("data type of answer is",type(z))
-# x= int(input("enter the first number: "))
-# y= int(input("enter the second number: "))
-# print("sum krna aand data types====",x+y,type(x+y))
-
-
-
-
-
-
-# QUESTION===21
-# num= input("enter a values:")
-# converted= float(num)
-# print("oringal values",type(num),num)
-# print("converted values",converted,type(converted))
-
-# num= "56"
-# num1= float(num)
-# print("Oringal values____",num,type(num))
-# print("changed values____",num1,type(num1))
-
-
-
-
-# ------------------- QUESTION BANK-------------------------#
-# Question 1.1 
-# Numbers a = 20, b = 5 lekar addition, subtraction, multiplication, division aur modulus print karo.
-# a= 20
-# b= 5
-# print("addition",a+b)
-# print("subtraction",a-b)
-# print("multiplication",a*b)
-# print("division",a/b)
-# print("modulus",a%b)
-
-# a= int(input("enter the first number:"))
-# b= int(input("enter the second number:"))
-# print("addition",a+b)
-# print("subtraction",a-b)
-# print("multiplication",a*b)
-# print("division",a/b)
-# print("modulus",a%b)
-
-
-# Question 1.2 
-# a = 15, b = 4 lekar all arithmetic operators ka result nikalo.
-# a= 15
-# b= 4
-# print("addition",a+b)
-# print("subtraction",a-b)
-# print("multiplication",a*b)
-# print("division",a/b)
-# print("modulus",a%b)
-
-# a= int(input("enter the first number:"))
-# b= int(input("enter the second number:"))
-# print("addition",a+b)
-# print("subtraction",a-b)
-# print("multiplication",a*b)
-# print("division",a/b)
-# print("modulus",a%b)
-
-
-
-# Question 1.3 
-# a = 10, b = 20 par relational operators (>, <, >=, <=, ==, !=) lagao.
-# a= 10
-# b= 20
-# print("this is grater than: ",a>b)
-# print("this is less than: ",a<b)
-# print("this is grater than equal equal: ",a>=b)
-# print("this is less than equal equal: ",a<=b)
-# print("this is equal equal: ",a==b)
-# print("this is not equal: ",a!=b)
-
-# a= int(input("enter the first values:"))
-# b= int(input("enter the second values:"))
-# print("this is grater than:",a>b)
-# print("this is less than:",a<b)
-# print("this is grater than equal:",a>=b)
-# print("this is less than equal:",a<=b)
-# print("this is ==",a==b)
-# print("this is !=",a!=b)
-
-
-
-
-# Question 1.4 
-# age = 18 aur hasID = True lekar logical AND (and / &&) use karke check karo ki person allowed hai ya nahi.
-# age =1
-# hasID =True
-# if(age >= 18 and hasID):
-#     print("person is allowed")
+# if age >= 18 and has_id:
+#     print("Person is Allowed")
 # else:
-#     print("person is not allowed") 
- 
-# age= int(input("enter the age: "))
-# hasID= True
-# if(age >=18 and hasID):
-#     print("person is allowed")
+#     print("Not Allowed")
+
+
+
+
+
+# age = 18
+# has_id = True
+
+# if age >= 18 and has_id:
+#     print("Person is Allowed")
 # else:
-#     print("person is not allowed")    
+#     print("Not Allowed")
 
 
 
-
-#Question 1.5 
-#Ek number lo aur check karo ki woh even hai ya odd, modulus operator % se.
-# a =18
-# if(a %2==0):
-#     print("this is even number")
+# num = 27
+# if(num % 2 == 0):
+#     print("Even")
 # else:
-#     print("this is odd number")
-#   
-# a =int(input("enter the number: "))
-# if(a %2 ==0):
-#     print("this is even number")
+#     print("Odd")   
+# 
+# 
+# 
+#
+# 
+# name = "Vishal"
+# age = 20
+# city = "Raipur"
+# print("Name:", name)
+# print("Age:", age)
+# print("City:", city) 
+
+
+# a = 10
+# b = 20
+# total_sum = a + b
+# print("Sum is:", total_sum)
+
+
+
+# length = 10
+# breadth = 5
+# area = length * breadth
+# print("Area of Rectangle:", area)
+
+
+# price = 500
+# quantity = 3
+# total_bill = price * quantity
+# print("Total Bill Amount:", total_bill)
+
+# score = 50
+# print("Before Update:", score)
+# score = 100  # Value updated
+# print("After Update:", score)
+
+
+
+
+# my_int = 10
+# my_float = 5.75
+# my_str = "Hello"
+# my_bool = True
+
+# print(type(my_int))
+# print(type(my_float))
+# print(type(my_str))
+# print(type(my_bool))
+
+
+
+
+# age_input = input("Enter age: ")  # input() hamesha string return karta hai
+# print("Type before conversion:", type(age_input))
+# age = int(age_input)  # Convert to integer
+# print("Type after int():", type(age))
+
+
+
+# price = 99.5
+# int_price = int(price)  # Decimal part remove ho jayega
+# print("Integer Price:", int_price)
+
+
+
+# my_list = [1, 2, 3]
+# my_tuple = (1, 2, 3)
+# my_dict = {"name": "Vishal", "age": 20}
+# my_set = {1, 2, 3}
+
+# print(type(my_list))
+# print(type(my_tuple))
+# print(type(my_dict))
+# print(type(my_set))
+
+
+# num = -15
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
 # else:
-#     print("this is odd number")      
+#     print("Zero")
 
 
 
-# Question 2.1 
-# Apna name, age aur city variables mein store karke print karo.
-# name ="vishal"
-# age =18
-# city ="Raipur"
-# print("my name is",name,"and i am",age,"year old my fav",city,"")
 
-# name =input("enter your name: ")
-# age= int(input("enter your age: "))
-# city= input("enter your city: ")
-# print("my name is",name)
-# print("i am ",age,"year old")
-# print("i am from",city)
-
-
-
-# Question 2.2
-# Do variables a = 10, b = 20 lekar unka sum print karo.
-# a= 10
-# b= 20
-# print("total sum",a+b)
-
-# a= int(input("enter the 1st number:"))
-# b= int(input("enter the 2nd number:"))
-# print("total number calculate",a+b)
-
-
-
-
-
-# Question 2.3 
-# length= 10, breadth= 5 lekar rectangle ka area nikalo.(Formula: Area = length * breadth)
-# length= 10
-# breath= 5
-# area =length * breath
-# print("rectangle of area:",area)
-
-# length= int(input("enter the number:"))
-# breadth= int(input("enter the number:"))
-# area= length * breadth
-# print("rantagle of area:",area)
-
-
-
-
-
-
-# Question 2.4
-# price = 500, quantity = 3 lekar total bill calculate karo.
-# price= 500
-# quantity= 3
-# print("otal bill calculate",price * quantity)
-
-# price =int(input("enter the price: "))
-# quantity=int(input("enter the quantity: "))
-# print("total bill calculate: ",price *quantity)
-
-
-
-
-
-
-
-# Question 2.5 
-# Ek variable ki value ko change/update karke before aur after value print karo.
-# score= 25
-# print("values of before:",score)
-# score= 100
-# print("values of after:",score)
-
-# score= int(input("enter the before values: "))
-# print("before",score)
-# score= int(input("enter the after values: "))
-# print("after values",score)
-
-
-
-
-
-
-
-# Question 3.1
-# Ek int, float, str aur bool variable banao aur unka type print karo
-# myint= 25
-# myfloat= 25.09
-# mystr= "vishal"
-# mybool= True
-# print("integer number:",type(myint))
-# print("my float number:",type(myfloat))
-# print("my mystr:",type(mystr))
-# print("my bool",type(mybool))
-
-# myint= int(input("enter your integer number: "))
-# myfloat= float(input("enter your floating number: "))
-# mystr=input("enter your sring name: ")
-# mybool= input("enter your boolean:").lower() == "true"
-
-# print("integer number:",type(myint))
-# print("my float number:",type(myfloat))
-# print("my mystr:",type(mystr))
-# print("my bool",type(mybool))
-
-
-
-
-# Question 3.2 
-# User se age input lo aur uska type check karo.
-# age =int(input("entr the age: "))
-# print("i am ",age," year old")
-# print("eska type hai yah: ",type(age))
-
-# age=18
-# print("your age:",age,"and datatype",type(age))
-
-
-
-# Question 3.3 
-# num = "100" ko integer mein convert karke 50 add karo.
-# num= "100"
-# convert=int(num)
-# print("convert",num)
-# print(convert+50)
-
-# num = input("Enter the str number: ")
-# convert = int(num)
-# print("Converted:", convert)
-# added = int(input("Enter the number to add: "))
-# print("Added number:", convert+ added)
-
-
-
-
-
-
-# Question 3.4 
-# price = 99.5 ko integer mein convert karke print karo.
-# price= 99.5
-# convert= int(price)
-# print("converted values:",convert)
-# a= float("enter the number:")
-# b= int(a)
-# print("converted",b)
-
-
-
-
-# Question 3.5
-# Ek list, tuple, dictionary aur set banao aur sabka type() print karo.
-# list1=["mungdal","chana","aalu"]
-# print("this is list",list1,"yah eska types hai",type(list1))
-
-
-# tuple1=("apple","banana","papaye")
-# print("this is tuple",tuple1,"yah eska types hai",type(tuple1))
-
-
-# dictionary={
-#     "name":"vishal",
-#     "age":18,
-#     "village":"bihar"
-# }
-# print("this is dictionary",dictionary,"yah eska types hai",type(dictionary))
-
-
-# set1={"rotti","chaval","tea"}
-# print("this is set",set1,"yah types",type(set1))
-
-
-
-
-
-# Question 4.1 
-# Number positive, negative ya zero hai, check karo.
-# a =0
-# if(a>0):
-#     print("this is positive number")
-# elif(a<0):
-#     print("this is negative number") 
+# num = 42
+# if num % 2 == 0:
+#     print("Even")
 # else:
-#     print("this is zero")       
+#     print("Odd")
 
 
 
-
-# Question 4.2
-# Number even ya odd hai, check karo.
-# a =18
-# if(a &2==0):
-#     print("this is even number")
+# age = 19
+# if age >= 18:
+#     print("Eligible")
 # else:
-#     print("this is odd number")  
+#     print("Not Eligible")
 
 
 
 
-# Question 4.3 
-# User ki age lekar check karo: 18 ya usse zyada → Eligible, warna Not Eligible.
-# age =17
-# if(age >= 18):
-#     print("eligible for voter")
+# a, b, c = 25, 40, 15
+# if a >= b and a >= c:
+#     print("Largest is:", a)
+# elif b >= a and b >= c:
+#     print("Largest is:", b)
 # else:
-#     print("not eligible for voter")    
+#     print("Largest is:", c)
 
 
 
-
-
-# Question 4.4 
-# Teen numbers mein se sabse bada number find karo.
-# a=10
-# b=1
-# c=2
-# if(a>=b and a>=c):
-#     print("first largest number")
-# elif(b>=a and b>=c):
-#     print("second largest number")
-# else:        
-#     print("third largest number")
-
-
-
-
-# Question 4.5 
-# Marks lekar grade print karo (90+ = A, 75–89 = B, 60–74 = C, 40–59 = D, <40 = Fail).
-# marks=29
-# if(marks>=90):
-#     print("Grade --'A'")
-# elif(marks>=75):
-#     print("Grade--B")
-# elif(marks>=64):
-#     print("Grade--c")
-# elif(marks>=40):
-#     print("Grade--D")
-# elif(marks>=30):
-#     print("Result is Passed")  
+# marks = 82
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 75:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# elif marks >= 40:
+#     print("Grade: D")
 # else:
-#     print("Result is Failled")       
+#     print("Grade: Fail")
 
-
-
-
-
-# Question 5.1 
-# 1 se 10 tak numbers print karo.
+for i in range(1, 11):
+    print(i, end=" ")
