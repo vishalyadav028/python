f14f769ee09a8d9d91964033cc72c4b4a6c6d4f9
Vishal Yadav <vishalyadav70563@gmail.com>
2026-09-09 23:45:33 +0530

    python

---
+++ all.py
@@ -0,0 +1,235 @@
+# a=10
+# b=20
+# print(a+b)
+
+
+
+# a=10
+# b=20
+# print(a-b)
+
+
+# a=10
+# b=20
+# print(a*b)
+
+
+
+# a=10
+# b=20
+# print(a/b)
+
+
+
+# a=10
+# b=20
+# print(a%b)
+
+
+
+# a=10
+# b=20
+# print(a**b)
+
+
+# ralational op
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+
+# a=10
+# a+=5
+# print(a)
+
+
+# arithmetic operator
+# a=int(input("enter the first no...."))
+# b=int(input("enter the first no...."))
+# print("addition: ",a+b)
+# print("subtraction: ",a-b)
+# print("multiplication: ",a*b)
+# print("division: ",a/b)
+# print("raminder: ",a%b)
+# print("power: ",a**b)
+
+
+
+# fruit=["apple","banana","mango"]
+# fruit[1]="orange"
+# print(fruit)
+# print(fruit[1])
+
+
+
+# student=("vishal","17","bca")
+# print(student[1])
+# print(student)
+
+
+
+# std=("vishal",17,"bca",17)
+# print(std)
+
+
+
+# num={10,20,30,40,50,50}
+# num.add(80)
+# num.remove(10)
+# print(num)
+
+
+
+
+# std={
+#     "name":"vishal",
+#     "age":18,
+#     "course":"bca"
+# }
+# print(std["name"])
+# std["city"]="raipur"
+# print(std)
+
+
+
+
+# age="18"
+# print(int(age))
+
+
+
+# num=10
+# # num=num+10
+# num =+10
+# print(num)
+
+
+# logical operator
+# a=50
+# b=30
+# print(not False)
+# print(not (a>b))
+
+
+
+
+
+# types conversion
+# a= float("2")
+# b= 4.25
+# print(type(b))
+# print(a+b)
+
+# a,b=1,2.0
+# sum =a+b
+# print(type(sum))
+
+# a= 3.14
+# b= str(a)
+# print(type(b))
+
+# x= 10
+# y= 2.5
+# print(x+y)
+# print(type(x+y))
+
+
+# TYPECASTING
+# age="20"
+# # age= int(age)
+# print(age)
+# print(type(age))
+
+
+
+# input lena sikha
+
+# name=input("enter your name: ")
+# print("welcome",name)
+
+
+# val= input("enter some values: ")
+# print(type(val),val)
+
+
+
+# val= float(input("enter some values:"))
+# print(type(val),val)
+
+
+# name= input("enter name: ")
+# age= int(input("enter age: "))
+# marks =float(input("enter marks: "))
+# print("welcome",name)
+# print("age",age)
+# print("marks",marks)
+
+
+
+
+# first= int(input("enter first: "))
+# second= int(input("enter second: "))
+# print("sum= ",first + second)
+
+# side= float(input("enter side: "))
+# print("area=",side*side)
+
+# a= float(input("enter first: "))
+# b= float(input("enter second: "))
+# print("avg=",(a+b)/2)
+
+
+
+# a= int(input("enter first: "))
+# b= int(input("enter second: "))
+# print(a>=b)
+
+
+
+# chapter 2 string
+
+# str1="this is a string"
+# str2='apna college'
+# str3='''i am vishal'''
+# print(str1,str2,str3)
+
+
+
+
+
+# str1="this is a string.\twe are creating it in python"
+# print(str1)
+
+
+
+
+# str1= "apna"
+# str2=" college"
+# sum= str1 + str2
+# print(sum)
+
+
+
+
+# str1='apna'
+# len1= len(str1)
+# print(len1)
+
+# str2='college'
+# len2=len(str2)
+# print(len2)
+
+# sum= str1+" "+str2
+# print(sum)
+# print(len(sum))
+
+
+
+
+
