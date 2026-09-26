a7e9a955083aec6632a567fa1aab95186a6e2d70
Vishal Yadav <vishalyadav70563@gmail.com>
2026-09-26 20:00:12 +0530

    Added Python code

---
+++ revision.py
@@ -0,0 +1,225 @@
+# print(("vishal kumar\n")*10)
+
+
+
+
+# name ="vishal kumar"
+# age1 =25
+# age2 =40
+# print(name)
+# print("actual values:",age2)
+# favsubject ="maths"
+# age2 =age1
+# print("changed values:",age2)
+
+
+
+# print("a simple program to understand input method in python")
+# name= input("enter your good name:")
+# print("your good name is:",name)
+
+
+
+# diameter =int(input("inter the values of diameter:"))
+# radius = diameter/2
+# area =3.14*(radius **2)
+# print("radius of the circle is",radius)
+# print("area of circle is",area)
+
+
+
+# food ="samosa"
+# age =25
+# area =670.98
+# name ="vishal"
+# print("data types of variable is", type(name))
+# print(type(area))
+
+
+
+
+# age =input("enter your age: ")
+# print("values entered is",age)
+# print("data type is",type(age))
+
+
+
+# input1= int(input("enter first number: "))
+# input2= int(input("enter second number: "))
+# sum= input1 + input2
+# print("sum of the 2 values is",sum)
+
+
+
+# x=5
+# y=10.5
+# z= x+y
+# print("answer is",z)
+# print("datatypes",type(z))
+
+
+
+
+
+# num= input("enter a values: ")
+# convertedvalues =float(num)
+# print("converted values",type(num))
+# print("data types",type(convertedvalues))
+
+
+
+
+# x=10
+# y=5
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+# print(x**y)
+
+
+
+
+
+# x= 10
+# y=5
+# print(x==y)
+# print(x!=y)
+# print(x<y)
+# print(x>y)
+# print(x<=y)
+# print(x>=y)
+
+
+
+
+# x=25
+# y=5
+# print(x>y and x<y)
+# print(x>y or x<y)
+# print(not (x<y))
+
+
+
+# str1='vishal'
+# str2="vishal"
+# str3='''vishal'''
+# print("first: ",str1)
+# print("second: ",str2)
+# print("third: ",str3)
+# # string concatenation
+# print(str1+" "+str2)
+# print(len(str1))
+
+
+
+
+
+# str1="vishal"
+# lenght= len(str1)
+# print(lenght)
+# print(str1[0])
+# print(str1[1])
+
+
+
+
+# str="gulabjamun"
+# first= str[0:5]
+# second= str[:5]
+# print(first)
+# print(second)
+
+
+
+
+
+
+
+# str= input("enter the values: ")
+# mid= len(str)//2
+# output1= str[mid-1:mid+2]
+# print(output1)
+
+
+
+
+# str="vishal kumar"
+# print(str.upper())
+# print(str.lower())
+# print(str.title())
+# print(str.find("h"))
+# print(str.replace("kumar","yadav"))
+# print(str.count("a"))
+
+
+# marks= 41
+# if(marks >=90):
+#     print("your grade is a")
+# elif(marks >=40):
+#     print("you grade is b")    
+
+
+
+# str="gulabjamun"
+# print(str[0:5])
+# print(str[:6])
+# print(str[5:])
+
+
+
+# marks=int(input("enter marks: "))
+# if marks >= 90:
+#     print("grade A")
+# elif marks>=80:
+#     print("grade B")
+# elif marks>=70:
+#     print("grade C")        
+# else:
+#     print("grade D")
+
+
+
+# num= int(input("enter a number: "))
+# if(num>0):
+#     print("positive")
+# elif(num==0):
+#     print("zero")
+# else:
+#     print("negative")        
+
+
+
+
+
+# food= ["chole bathure","choco waffle","gulab jamun"]
+# print(len(food))
+
+# print("first values of the list: ",food[0])
+# print("fisrt values of the list: ",food[1])
+
+
+# marks=[99,100,90,95]
+# print(marks)
+# marks[1]=98
+# print(marks)
+# name="vishal"
+# print(name)
+
+
+
+
+# print("hello")
+# print("10/0")
+# print("bye")
+
+
+
+# constance
+# PI =23
+# convert =float(23)
+# print(type(convert))
+
+
+
